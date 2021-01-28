a86ddc6b2df9bd2c268341a3177b4ce959e1738b
ghesketh76 <ghesketh76@gmail.com>
2021-01-28 16:16:21 -0700

    added trending controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   resources :ratings
   resources :activities
+
+  get '/activities_trending', to: 'activities#trending_activities'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
